31c75248369ce693454c023a52a9125e3808d91c
Aleksander Malaszkiewicz <alek@fractalsoft.org>
2014-05-11 00:52:21 +0200

    Clean up CodesController using decent_exposure

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,5 +1,7 @@
+# Codes controller
 class CodesController < ApplicationController
-  before_action :set_code, only: [:show, :edit, :update, :destroy]
+  respond_to :html
+  expose(:code, attributes: :code_params)
 
   # GET /codes
   # GET /codes.json
@@ -7,68 +9,31 @@ class CodesController < ApplicationController
     @codes = Code.where(parent_id: nil)
   end
 
-  # GET /codes/1
-  # GET /codes/1.json
-  def show
-  end
-
-  # GET /codes/new
-  def new
-    @code = Code.new
-  end
-
-  # GET /codes/1/edit
-  def edit
-  end
-
   # POST /codes
   # POST /codes.json
   def create
-    @code = Code.new(code_params)
-
-    respond_to do |format|
-      if @code.save
-        format.html { redirect_to @code.parent || @code, notice: 'Code was successfully created.' }
-        format.json { render :show, status: :created, location: @code }
-      else
-        format.html { render :new }
-        format.json { render json: @code.errors, status: :unprocessable_entity }
-      end
-    end
+    code.save
+    respond_with code
   end
 
   # PATCH/PUT /codes/1
   # PATCH/PUT /codes/1.json
   def update
-    respond_to do |format|
-      if @code.update(code_params)
-        format.html { redirect_to @code, notice: 'Code was successfully updated.' }
-        format.json { render :show, status: :ok, location: @code }
-      else
-        format.html { render :edit }
-        format.json { render json: @code.errors, status: :unprocessable_entity }
-      end
-    end
+    code.save
+    respond_with code
   end
 
   # DELETE /codes/1
   # DELETE /codes/1.json
   def destroy
-    @code.destroy
-    respond_to do |format|
-      format.html { redirect_to codes_url, notice: 'Code was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    code.destroy
+    respond_with code
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_code
-      @code = Code.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def code_params
-      params.require(:code).permit(:user, :source, :parent_id)
-    end
+  # Never trust parameters from the scary internet.
+  def code_params
+    params.require(:code).permit(:user, :source, :parent_id)
+  end
 end
--- app/helpers/codes_helper.rb
+++ app/helpers/codes_helper.rb
@@ -1,11 +1,12 @@
 # Codes helper
 module CodesHelper
   def markdown(text)
-    options = { autolink: true, no_intra_emphasis: true }
-    render_options = { hard_wrap: true, filter_html: true }
-    render = Redcarpet::Render::HTML.new(render_options)
-    markdown = Redcarpet::Markdown.new(render, options)
-    syntax_highligter(markdown.render(text)).html_safe
+    text
+    # options = { autolink: true, no_intra_emphasis: true }
+    # render_options = { hard_wrap: true, filter_html: true }
+    # render = Redcarpet::Render::HTML.new(render_options)
+    # markdown = Redcarpet::Markdown.new(render, options)
+    # syntax_highligter(markdown.render(text)).html_safe
   end
 
   def syntax_highligter(html)
